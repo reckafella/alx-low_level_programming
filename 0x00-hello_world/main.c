16b7bcdc9f49d225f77672536188adcad88b32db
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-16 06:01:00 -0800

    add 0-preprocessor

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* This is a test program for checking if the bash scripts fail or pass */
+
+int main(void)
+{
+  printf("Hello, World!\n");
+  printf("Happy to be here? Yes, happy to be here!\n")
+}
