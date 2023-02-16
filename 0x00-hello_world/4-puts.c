9c140187d30faa8434abfcdc9c0cc0ed97e3e74a
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-16 10:05:42 -0800

    edit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
-/* The main function does not  program prints a line of text using 'puts()'. */
+/**
+* main - Entry point
+* Function prints a string usings 'puts()'
+* Escape sequences required to ensure doubles quotes are printed out.
+*
+*Return: 0 if function works;
+*/
 
 int main(void)
 {
-	/*
-	 * main - Entry point
-	 */
-	/*
-	 * Function prints a string usings 'puts()'
-	 * Escape sequences required to ensure doubles quotes are printed out.
-	 */
-
 	char *str_ = "\"Programming is like building a multilingual puzzle";
 
 	puts(str_);
-
-	return (0);
 }
