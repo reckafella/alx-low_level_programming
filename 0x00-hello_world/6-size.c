483bc71455c695acb9f110bd79e57652a20efb76
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-16 11:43:26 -0800

    edit 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,19 +10,17 @@
 
 int main(void)
 {
-	int integer;
-	long l_int;
-	long long int ll_int;
-	char char_;
-	float flo_at;
-	double dou_ble;
+	int i;
+	long li;
+	long lli;
+	char c;
+	float f;
 
-	printf("Size of an int: %ld byte(s)\n", (long int) sizeof(integer));
-	printf("Size of a char: %ld byte(s)\n", (long int) sizeof(char_));
-	printf("Size of a long int: %ld byte(s)\n", (long int) sizeof(l_int));
-	printf("Size of a long long int: %ld byte(s)\n", (long int) sizeof(ll_int));
-	printf("Size of a float: %ld byte(s)\n", (long int) sizeof(flo_at));
-	printf("Size of a double: %ld byte(s)\n", (long int) sizeof(dou_ble));
+	printf("Size of an int: %ld byte(s)\n", (long int) sizeof(i));
+	printf("Size of a char: %ld byte(s)\n", (long int) sizeof(c));
+	printf("Size of a long int: %ld byte(s)\n", (long int) sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", (long int) sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", (long int) sizeof(f));
 
 	return (0);
 
