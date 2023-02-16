3ba3396daa1f95a105cd17b3dbe0aa170b7eb947
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-16 10:22:35 -0800

    add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: main function uses printf to print out a line of text.
+ *
+ * Return: 0 if the function works as expected.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
