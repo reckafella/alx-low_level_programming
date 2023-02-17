75ec373f9c764dc0a30cd4065aa4e42e802e8e6a
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-17 12:43:32 -0800

    add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	char upp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	int j;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(str[i]);
+	}
+	
+	while (j < 26)
+	{
+		putchar(upp[j]);
+		j++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
