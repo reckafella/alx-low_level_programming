6740448ed0b70292cc0d62aa95fa018f13abf219
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-17 14:19:52 -0800

    add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
