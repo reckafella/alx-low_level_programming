9cbcd2655c9d31851503b348dd2b32b79dc3ce3f
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-21 12:57:11 -0800

    add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of passed value
+ * @n: integer passed to the function
+ *
+ * Return: 1 if positive. -1 if negative. 0 otherwise.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(45);
+		_putchar(44);
+		_putchar(32);
+		_putchar('\n');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		_putchar(44);
+		_putchar(32);
+		_putchar('\n');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		_putchar('\n');
+		return (0);
+	}
+}
