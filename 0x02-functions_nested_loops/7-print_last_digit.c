31363b2cd908a66e8cfcc4b98c0210efd2b15351
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-21 14:17:20 -0800

    add 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of an integer value
+ * @n: value passed to the function
+ *
+ * Return: 
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = (n % 10);
+	write(1, last_digit, 1);
+}
