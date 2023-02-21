de34c1aef8363024cbec0e5a14c590a225422147
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-21 06:14:59 -0800

    add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: prints "_putchar" followed by a new line using write()
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char *ch = "_putchar\n";
+	int length = 9;
+
+	write(1, ch, length);
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,6 @@
 ## Working with Functions and loops
+
+0. _putchar
+Write a program that prints _putchar, followed by a new line.
+
+The program should return 0
