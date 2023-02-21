2dbededdc7d590cdc091962201fdcf89870bee8c
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-21 08:09:04 -0800

    add 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times followed by new line
+ * return: no return value
+ */
+
+void print_alphabet_x10(void)
+{
+	char *alphabet = "abcdefghijklmnopqrstuvwxyz"
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(alphabet[j]);
+		}
+		_putchar('\n');
+	}
