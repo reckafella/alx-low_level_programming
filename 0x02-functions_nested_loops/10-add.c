6cf42320d603af6f124c3bfdb3506c9a5fc0e674
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-21 17:33:24 -0800

    add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integers and print out the result
+ * @x: first integer argument
+ * @y: second integer argument
+ *
+ * Return: 0
+ */
+
+int add(int x, int y)
+{
+	int sum = (x + y);
+
+	printf("%d", sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,3 +12,4 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void print_to_98(int n);
+int add(int, int);
