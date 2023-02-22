501fca1c7b38dd10a3186edc720f985e64ee19de
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-23 01:35:44 +0300

    update 100-print_times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,6 +3,54 @@
 #include <string.h>
 
 /**
+* print_tables - print tables
+* @n: parameter
+* Return: 0
+*/
+
+int print_tables(int n)
+{
+	product = (i * j);
+
+	if (j == 0)
+	{
+		_putchar(product + '0');
+	}
+	else if (product < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(product + '0');
+	}
+	else if (product < 100)
+	{
+		first = (product / 10);
+		second = (product % 10);
+
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(first + '0');
+		_putchar(second + '0');
+	}
+	else
+	{
+		first = (product / 100);
+		second = ((product / 10) % 10);
+		third = (product % 10);
+
+		_putchar(' ');
+		_putchar(first + '0');
+		_putchar(second + '0');
+		_putchar(third + '0');
+	}
+	if (j >= 0 && j < n)
+	{
+		_putchar(',');
+	}
+}
+
+/**
  * print_times_table - print n*n multiplication table
  * @n: parameter
  *
@@ -24,46 +72,7 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				product = (i * j);
-
-				if (j == 0)
-				{
-					_putchar(product + '0');
-				}
-				else if (product < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
-				}
-				else if (product < 100)
-				{
-					first = (product / 10);
-					second = (product % 10);
-
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(first + '0');
-					_putchar(second + '0');
-				}
-				else
-				{
-					first = (product / 100);
-					second = ((product / 10) % 10);
-					third = (product % 10);
-
-					_putchar(' ');
-					_putchar(first + '0');
-					_putchar(second + '0');
-					_putchar(third + '0');
-
-				}
-
-				if (j >= 0 && j < n)
-				{
-					_putchar(',');
-				}
+				print_tables(n);
 			}
 			_putchar('\n');
 		}
