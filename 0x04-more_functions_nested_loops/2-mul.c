c98e78b3397bafb2c6598099849770965e1f2bc3
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-23 14:53:13 -0800

    add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers and return the result;
+ *@a: first operand
+ *@b: second operand
+ *
+ * return: result
+ */
+
+
+int mul(int a, int b)
+{
+	int result;
+	
+	result = (a * b);
+	return (result);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -13,4 +13,9 @@ Write a function that checks for a digit (0 through 9).
  - Returns 1 if c is a digit
  - Returns 0 otherwise
 
+2. Collaboration is multiplication
+Write a function that multiplies two integers.
+
+ - Prototype: int mul(int a, int b);
+
 
