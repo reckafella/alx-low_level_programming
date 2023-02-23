0a794a38fb0eec833f00c4820d9e72a6726ec328
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-23 15:25:19 -0800

    add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 - 9 followed by a new line
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+	int j = 57;
+
+	for (i = 48; i <= j; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
