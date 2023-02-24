ef77583900b0de76e9e3f51aeae97a20276f19f7
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-23 17:32:35 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void fizz_buzz(void);
+
+/**
+ * main - entry point
+ * Return: 0
+*/
+
+int main(void)
+{
+	fizz_buzz();
+
+	return (0);
+}
+
+/**
+ * fizz_buzz - print numbers from 1 to 100
+ *
+ * Return: 0
+*/
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
