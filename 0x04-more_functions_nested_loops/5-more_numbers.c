8297c81d67a43eb1dc16b30440f9c9baabfa263e
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-24 12:52:53 -0800

    edit 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,22 +11,17 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	char *nl = "\n";
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
+			if (j >= 10)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
-			else
-			{
-				_putchar(j + '0');
-			}
-			write(1, nl, 1);
+			_putchar('0' + (j % 10));
 		}
+		_putchar('\n');
 	}
 }
