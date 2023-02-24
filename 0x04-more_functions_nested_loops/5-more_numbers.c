4959ef4b776d8903a7f0be8f08f3dcc46d504104
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-24 12:37:24 -0800

    edit 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,6 +2,29 @@
 #include "main.h"
 
 /**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	more_numbers();
+
+	return (0);
+}
+
+/**
+ * _putchar - print a single character
+ * @c: character to print
+ *
+ * Return: printed character
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * more_numbers - print 0-14 10 times
  *
  * return: void
