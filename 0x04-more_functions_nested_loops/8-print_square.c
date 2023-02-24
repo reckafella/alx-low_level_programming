e89fe157094f52169b098c7d2bf36f5eab0bad37
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-23 17:11:34 -0800

    edit 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,19 @@ void print_square(int size)
 	int row;
 	int column;
 
-	for (row = 0; row < size; row++)
-	{
-		for (column = 0; column < size; column++)
-		{
-			_putchar('#');
-		}
+	if (size <= 0)
+    {
 		_putchar('\n');
+    }
+    else
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
