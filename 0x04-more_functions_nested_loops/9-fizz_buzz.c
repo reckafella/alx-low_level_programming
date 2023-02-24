408020cca93fc53a1920e83437f4e51b54f25b12
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-23 17:42:03 -0800

    edit fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -29,21 +29,22 @@ void fizz_buzz(void)
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if ((i % 3) == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", i);
+		}
+
+		if (i >= 1 && i < 100)
+		{
 			printf(" ");
 		}
 	}
