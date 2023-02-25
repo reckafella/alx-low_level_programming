597b763383da15afbed84b69fa7c621e44544a86
The Accountant <ethanmuthoniw6@gmail.com>
2023-02-25 06:09:35 -0800

    edit 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,15 +15,10 @@ void print_number(int n)
 		n = -1 * n;
 	}
 
-	if (n == 0)
-	{
-		_putchar(n + '0');
-	}
-
-	if ((n / 10))
+	if ((n / 10) != 0)
 	{
 		print_number(n / 10);
 	}
-	_putchar((n % 10) + '0');
 
+	_putchar((n % 10) + '0');
 }
