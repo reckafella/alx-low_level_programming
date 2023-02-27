4d72b065ee23b6033a1663898e486800de5e34e5
reckafella <ethanmuthoniw6@gmail.com>
2023-02-28 00:09:43 +0300

    add 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string.
+ * @s: string parameter
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -6,4 +6,7 @@
 2. 1-swap.c - a function that swaps the values of two integers
  - Prototype: void swap_int(int *a, int *b);
 
+3. 2-strlen.c - a function that returns the length of a string.
+
+ - Prototype: int _strlen(char *s);
 
