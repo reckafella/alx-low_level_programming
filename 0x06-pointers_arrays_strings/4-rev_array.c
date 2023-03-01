f5728a2e4fc20be997b571c5a6215329f193e5a5
reckafella <ethanmuthoniw6@gmail.com>
2023-03-02 00:23:24 +0300

    edit 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,14 +13,13 @@ void reverse_array(int *a, int n)
 	int i;
 	int tmp_char;
 
-	n--; /* decrement n given that index starts at 0.*/
+	n = (n / 2); /* decrement n given that index starts at 0.*/
 
 	while (i < n)
 	{
 		tmp_char = a[i];
-		a[i] = a[n];
-		a[n] = tmp_char;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp_char;
 		i++;
-		n--;
 	}
 }
