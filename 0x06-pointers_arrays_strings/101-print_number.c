629c29090c46ca34b0a1c0dc2e69a7312bb17c9e
reckafella <ethanmuthoniw6@gmail.com>
2023-03-02 23:47:11 +0300

    edit 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,20 +9,21 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	int num = n;
+	if (num < 0)
 	{
 		_putchar('-');
-		n = ((-1) * n);
+		num = ((-1) * num);
 	}
 
-	if (n == 0)
+	if (num == 0)
 	{
-		_putchar(n);
+		_putchar(num);
 	}
 
-	if ((n / 10))
+	if ((num / 10))
 	{
-		print_number((n / 10));
+		print_number((num / 10));
 	}
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
