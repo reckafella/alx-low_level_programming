cf09c5f860a219cda1b4ada8a9b7f8850f4825b9
reckafella <ethanmuthoniw6@gmail.com>
2023-03-02 23:26:08 +0300

    add 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: integer value
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = (n * -1);
+
+	/**
+	 * When number has two or more digits,
+	 * pass it through the function recursively
+	 * till we get a single digit value
+	 */
+
+	if ((n / 10) != 0)
+	{
+		print_number((n / 10));
+	}
+	_putchar((n % 10) + '0');
+}
