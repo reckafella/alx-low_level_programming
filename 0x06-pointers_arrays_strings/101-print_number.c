b257b929da8cc70a4d4ad8602da13247f3f83a9f
reckafella <ethanmuthoniw6@gmail.com>
2023-03-02 23:33:18 +0300

    edit 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,16 +12,20 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = (n * -1);
+		n = ((-1) * n);
 	}
 
+	if (n == 0)
+	{
+		_putchar(n + '0');
+	}
 	/**
 	 * When number has two or more digits,
 	 * pass it through the function recursively
 	 * till we get a single digit value
 	 */
 
-	if ((n / 10) != 0)
+	if ((n / 10))
 	{
 		print_number((n / 10));
 	}
