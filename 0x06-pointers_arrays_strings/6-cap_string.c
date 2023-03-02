fa5a3a65db1ab8d9a4ac68eff440bd7280dd3e3e
reckafella <ethanmuthoniw6@gmail.com>
2023-03-02 22:47:55 +0300

    edit 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,15 +16,28 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0)
+		if (s[i] == '!' || s[i] == '.' || s[i] == '?')
 		{
-			if (s[i] >= 'a' && s[i] <= 'z')
+			*s++;
+			if (s[i] == ' ')
 			{
-				s[i] = (s[i] - 32);
+				*s++;
 			}
-			continue;
+			s[i] = (s[i] - 32);
 		}
-		if (s[i] == (sp[j]))
+		else if (s[i] == '\n' || s[i] == '\t' || s[i] == '"')
+		{
+			*s++;
+		}
+		else if (s[i] == ',' || s[i] == ';' || s[i] == '(')
+		{
+			*s++;
+		}
+		else if (s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			*s++;
+		}
+		else
 		{
 			continue;
 		}
