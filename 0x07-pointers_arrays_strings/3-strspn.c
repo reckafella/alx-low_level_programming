7f074b43e99d53359be336ace7ccaae9b11b0e4c
reckafella <ethanmuthoniw6@gmail.com>
2023-03-06 23:50:29 +0300

    add 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strspn - return length of a substring
+ * @s: pointer to a string
+ * @accept: pointer to a string
+ *
+ * Return: integer value
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int sublen;
+	sublen = 0;
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		for (j = 0; j < _strlen(accept); j++)
+		{
+			if (s[i] == accept[j])
+			{
+				sublen++;
+			}
+		}
+	}
+}
+/**
+ * _strlen - returns the string length
+ * @str: string
+ *
+ * Return: string length
+ */
+
+unsigned int _strlen(char *str)
+{
+	unsigned int len;
+	len = 0;
+
+        /* Get string length */
+        while (str[len] != '\0')
+        {
+                len++;
+        }
+
+	return (len);
+}
