1c1ac1b748e2a124c2a6910488a398cde257c979
reckafella <ethanmuthoniw6@gmail.com>
2023-03-06 17:31:54 +0300

    edit 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,7 +11,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int j;
+	unsigned int j;
 
 	for (j = 0; j < n; j++)
 	{
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,4 @@
-ifndef MAIN_H
-define MAIN_H
-
 #include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
 
-endif /* MAIN_H */
