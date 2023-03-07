854231ac18587068c31ca481aaab68f5075bd059
reckafella <ethanmuthoniw6@gmail.com>
2023-03-07 17:04:06 +0300

    add 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the string length
+ * @str: string
+ *
+ * Return: string length
+ */
+
+unsigned int _strlen(char *str)
+{
+	unsigned int len = 0;
+
+	/* Get string length */
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * _strstr - find the first occurrence of a substring.
+ * @haystack: string to within which to search
+ * @needle: substring to search
+ *
+ * Return: pointer to beginning of the substring.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int haystack_len = _strlen(haystack);
+	unsigned int needle_len = _strlen(needle);
+
+	for (i = 0; i < haystack_len && haystack[i] != '\0'; i++)
+	{
+		for (j = 0; j < needle_len && needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 unsigned int _strlen(char *s);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
