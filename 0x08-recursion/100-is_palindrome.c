3f4e9349333cee85319c8f07e5fcc96d751ee38c
reckafella <ethanmuthoniw6@gmail.com>
2023-03-09 02:55:26 +0300

    update palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse a string recursively
+ * @s: string
+ *
+ * Return: string
+ */
+
+char _rev_recursion(char *s)
+{
+	if (*s)
+	{
+		return (_print_rev_recursion(s + 1));
+	}
+}
+
+/**
+ * _strlen_recursive - recursively determine string length
+ * @s: string input
+ *
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length = (1 + _strlen_recursion(s + 1));
+		return (length);
+	}
+	return (0);
+}
+
+
+/**
+ * is_palindrome - return 1 if a string a palindrome, 0 otherwise
+ * @s: string
+ *
+ * Return: int
+ */
+
+int is_palindrome(char *s)
+{
+	if (_strlen_recursive(s) == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		if (_rev_recursion(s) == (s))
+		{
+			is_palindrome(s + 1);
+			return (1);
+		}
+		else
+		{
+			is_palindrome(s + 1);
+			return (0);
+		}
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+char _rev_recursion(char *s)
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -6,3 +7,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
