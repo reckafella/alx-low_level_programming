0554eb8ed0ced97929c7b7aea6ea4d7f9e1e09f4
reckafella <ethanmuthoniw6@gmail.com>
2023-03-09 03:36:39 +0300

    edit 100-palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,17 +7,15 @@
  * Return: void
  */
 
-char puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return (s);
+		return;
 	}
 
 	_putchar(*s);
 	_puts_recursion(s + 1);
-
-	return (s);
 }
 
 /**
@@ -27,14 +25,13 @@ char puts_recursion(char *s)
  * Return: void
  */
 
-char print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	return (s);
 }
 /**
  * _strlen_recursion - recursively determine string length
@@ -65,21 +62,17 @@ int _strlen_recursion(char *s)
 
 int is_palindrome(char *s)
 {
-	if (_strlen_recursion(s) == 0)
+	int len = _strlen_recursion(s);
+
+	if (len == 0)
 	{
 		return (1);
 	}
-	else
+	if (*s == *(s + len - 1))
 	{
-		if (print_rev_recursion(s) == puts_recursion(s))
-		{
-			return (1);
-		}
-		else
-		{	
-			return (0);
-		}
+		*(s + len - 1) = '\0';
+		return (is_palindrome(s + 1));
 	}
-	is_palindrome(s + 1);
+
 	return (0);
 }
