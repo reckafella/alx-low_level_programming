df8c4e8a5cfbaf1d1d8fa07b9fbaad48b600d00c
ethan muthoni <ethan.muthoni@pm.me>
2023-03-21 02:11:14 +0300

    edit dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+ * struct dog - new structure with following members
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
 struct dog
 {
 	char *name;
