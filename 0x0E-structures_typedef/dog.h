21291b307b41d77129ea35587e4f7dbee9992068
ethan muthoni <ethan.muthoni@pm.me>
2023-03-21 03:41:32 +0300

    update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,21 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - new structure using typedef
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
