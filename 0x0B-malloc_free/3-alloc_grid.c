49acbe94cbd86957cae7bffbe235d1260c62d140
ethan muthoni <ethan.muthoni@pm.me>
2023-03-22 19:06:15 +0300

    edit 3-alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,40 +15,40 @@ int **alloc_grid(int width, int height)
 	int i;
 	int j;
 	int **arr;
-	int k;
-
-	arr = malloc(sizeof(int *) * height);
-
-	if (arr == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		for (i = 0; i < height; i++)
-		{
-			arr[i] = malloc(sizeof(int) * width);
-		}
-	}
 
 	if (height <= 0 || width <= 0)
 		return (NULL);
 
-	/* Initialize all memory locations with zero (0) */
-	for (j = 0; j < height; j++)
+	arr = malloc(height * sizeof(int *));
+
+	if (!arr)
+		return (NULL);
+
+	/**
+	 * if step above passes, allocate memory for each 1D array
+	 * if at any point, a 1D array is NULL,
+	 * Deallocate all previously allocated memory
+	 */
+
+	for (i = 0; i < height; i++)
 	{
-		for (k = 0; k < width; k++)
+		arr[i] = malloc(width * sizeof(int));
+
+		if (!(arr[i]))
 		{
-			arr[j][k] = 0;
+			for (j = 0; j < i; j++)
+				free(arr[j]);
+			free(arr);
+
+			return (NULL);
 		}
 	}
 
-	/* Deallocate memory if malloc fails */
-	if (!arr)
-	{
-		for (i = 0; i < height; i++)
-			free(arr[i]);
-	}
+	/* Initialize all memory locations with zero (0) */
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
 
 	return (arr);
 }
