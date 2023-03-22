5f32e7819f3b8f7e6c91eb445413343ced9b7ad7
ethan muthoni <ethan.muthoni@pm.me>
2023-03-23 01:55:33 +0300

    add 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - returns index of first element of integer type
+ * @array: array to search
+ * @size: array size
+ * @cmp: pointer to the comparison function
+ *
+ * Return: integer index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!(array))
+		exit(98);
+
+	if (!(cmp))
+		exit(98);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if((*cmp)(array[i]))
+			return (i);
+
+	/* if no element is found, return -1 */
+	return (-1);
+}
