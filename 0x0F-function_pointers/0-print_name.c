e4298641489df7d671744a28edab8eb9ddb05b8f
ethan muthoni <ethan.muthoni@pm.me>
2023-03-23 00:59:20 +0300

    edit 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,9 +5,11 @@
  * print_name - a function that prints a name
  * @name: name to be print to standard output
  * @f: pointer to a function
+ *
+ * Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	(*f)(name);
 }
