67c905800277c50319f571516a7ec43b473c1857
ethan muthoni <ethan.muthoni@pm.me>
2023-03-23 00:08:02 +0300

    add 0-print_name.c, README file, and function_pointers.h

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - a function that prints a name
+ * @name: name to be print to standard output
+ * @f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,5 @@
+### 0x0F. C - Function pointers
+# Project Tasks
+
+1. 0-print_name.c -- a function that prints a name
+ - Prototype: void print_name(char *name, void (*f)(char *));
