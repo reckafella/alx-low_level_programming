4bc299e5c46dad10a7247cec109bb3f9755cbcd5
reckafella <ethanmuthoniw6@gmail.com>
2023-03-24 04:40:25 +0300

    add 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print each number in an argument, followed by a
+ * separator and a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+
+		if (!(separator))
+			continue;
+		else
+			if (i < (n -1))
+				printf("%s", separator);
+	}
+
+	va_end(ap);
+
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -6,4 +6,11 @@
  - Prototype: int sum_them_all(const unsigned int n, ...);
  - If n == 0, return 0
 
-2. 
+2. 1-print_numbers.c -- a function that prints numbers, followed by a new line.
+
+ - Prototype: void print_numbers(const char *separator, const unsigned int n, ...);
+ - where separator is the string to be printed between numbers
+ - and n is the number of integers passed to the function
+ - You are allowed to use printf
+ - If separator is NULL, don’t print it
+ - Print a new line at the end of your function 
