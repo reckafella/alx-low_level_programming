635b8e35e7b6fcb902a152bbe4f9a2353df836e0
reckafella <ethanmuthoniw6@gmail.com>
2023-04-11 02:34:57 +0300

    edit 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,27 +3,17 @@
 /**
  * print_binary - a function printing a decimal number in binary format
  * @n: an unsigned long integer to print
- * 
+ *
  * Return: nothing
  */
 
 void print_binary(unsigned long int n)
 {
-	int c;
-	int binary;
-
-	if (n == 0)
+	if (n > 1)
+		print_binary(n >> 1);
+	
+	if (n & 1)
+		_putchar('1');
+	else
 		_putchar('0');
-	else if (n > 0)
-	{
-		for (c = 32; c >= 0; c--)
-		{
-			binary = n >> c;
-
-			if (binary & 1)
-				_putchar('1');
-			else
-				_putchar('0');
-		}
-	}
 }
