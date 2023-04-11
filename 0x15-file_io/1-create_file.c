25f4da691498c941355bc38433879d394c39938f
ethan muthoni <ethan.muthoni@pm.me>
2023-04-12 02:17:30 +0300

    add 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * create_file -  create a text file
+ * @filename: name of the file to create
+ * @text_content: a null terminated string
+ *
+ * Return: 1 on success; -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_TRUNC, 600);
+
+	if (!fd)
+		return (-1);
+
+	/*Add content to the new file */
+	if (!(write(fd, text_content, len)))
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
+
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: string input
+ *
+ * Return: string length (int)
+ */
+
+int _strlen(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen(s + 1));
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,8 +8,11 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 /* Function prototypes */
+int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
