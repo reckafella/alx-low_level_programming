bf1bf6f04e19d95678bcad8b74837c2a213b3de7
ethan muthoni <ethan.muthoni@pm.me>
2023-04-15 21:06:09 +0300

    edit 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,10 +22,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content == NULL || text_content[0] == '\0')
 	{
-		close(fd);
-		return (1);
+		if (!(fd == -1))
+		{
+			close(fd);
+			return (1);
+		}
+
+		return (-1);
 	}
 
 	if (len > 0)
@@ -37,6 +42,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
+
 	close(fd);
 	return (1);
 }
