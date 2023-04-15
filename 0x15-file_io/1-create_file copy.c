50dbe4a0a54358febf49f6db6fbfcfe763426b0b
ethan muthoni <ethan.muthoni@pm.me>
2023-04-15 12:03:00 +0300

    edit 1-create_file.c

--- 0x15-file_io/1-create_file copy.c
+++ 0x15-file_io/1-create_file copy.c
@@ -12,33 +12,18 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int len = _strlen(text_content);
-	ssize_t bytes_written = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_TRUNC, 600);
 
-	if (fd == -1)
+	if (!fd)
 		return (-1);
 
 	/*Add content to the new file */
-	if (len > 0)
-	{
-		bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-
-	/* check if writing succeeded */
-	if (fsync(fd) == -1)
-	{
-		close(fd);
-		return (0);
-	}
+	if (!(write(fd, text_content, len)))
+		return (-1);
 
 	close(fd);
 
