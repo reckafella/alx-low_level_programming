814b9f664b35bba42b3b960fede0e1c68ad4e6a4
ethan muthoni <ethan.muthoni@pm.me>
2023-04-21 13:11:12 +0300

    edit 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * set_bit - a function that sets a bit to 1 at a given index
+ * clear_bit - a function that sets a bit to 1 at a given -> index
  * @n: unsigned long int
  * @index: position
  *
  * Return: 1 on success or -1 on failure
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int mask = 1UL;
