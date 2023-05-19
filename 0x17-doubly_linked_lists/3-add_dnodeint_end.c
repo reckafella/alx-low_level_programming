fedc1655580132593a4053c8e7f20d5ee4dedffd
ethan muthoni <ethan.muthoni@pm.me>
2023-05-19 05:52:02 +0300

    Add 3-add_node_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,29 +13,25 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *ptr = *head;
 
 	new_node = malloc(sizeof(dlistint_t));
+
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
-
-	if (!(new_node->n))
-	{
-		free(new_node);
-		return (NULL);
-	}
-
 	new_node->next = NULL;
 
-	if (!(*head))
+	if (*head == NULL)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
+		return (new_node);
 	}
 
-	while (ptr->next)
+	while (ptr->next != NULL)
 		ptr = ptr->next;
 
 	ptr->next = new_node;
 	new_node->prev = ptr;
+
 	return (new_node);
 }
