0d07cc6ca3992ad74d50c490ce1bbbf68b8ca12c
ethan muthoni <ethan.muthoni@pm.me>
2023-05-19 05:13:06 +0300

    check if code works

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->n = n;
 
-	if (new_node->n == n)
+	if (new_node->n)
 	{
 		new_node->next = (*head);
 		new_node->prev = NULL;
