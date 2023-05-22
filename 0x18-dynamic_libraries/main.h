a2e2cf334e713a4f95902097a4fe0c3d849629fa
The Accountant <ethanmuthoniw6@gmail.com>
2023-05-22 12:27:53 -0700

    bored

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifndef EXIT_SUCCESS
+#define EXIT_SUCCESS 0
+#endif
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +25,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif /* MAIN_H */
