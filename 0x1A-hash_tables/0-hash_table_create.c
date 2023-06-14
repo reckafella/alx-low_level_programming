e2f964b3403a31ce8520462f6f45a29adcc2e3b7
ethan muthoni <ethan.muthoni@pm.me>
2023-06-14 18:33:40 +0300

    add 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: size of the hash table
+ *
+ * Return: pointer to address of the created table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+	unsigned long int i;
+
+	/* Allocate memory on the heap using malloc. Return NULL if malloc fails. */
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+
+	/* Allocate memory for each array. Return NULL if calloc fails. */
+	table->array = calloc(table->size, sizeof(hash_node_t *));
+	if (!table->array)
+		return (NULL);
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
