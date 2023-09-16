c0db6d768224e920cf4e1cdbde6b3b8b98d78b36
reckafella <ethanmuthoniw6@gmail.com>
2023-09-16 14:38:08 +0300

    Resolve issues with binary_search function

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,16 +12,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int index = 0;
+	size_t i;
 
 	if (array == NULL)
-		return (-1)
-	
-	for (; index < size; index++)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		if (*array == value)
-			return (index)
-		else
-			return (-1)
+		printf("Value checked array[%li] = [%i]\n", i, *(array + i));
+		if (*(array + i) == value)
+			return (i);
 	}
+	return (-1);
 }
